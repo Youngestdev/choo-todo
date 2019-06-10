20f6fb76c5cd6866d8f488b8b5619328121509e9
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-06-10 07:06:50 +0100

    Perhaps we have a SW bug?

--- sw.js
+++ sw.js
@@ -4,12 +4,14 @@ var VERSION = require('./package.json').version
 var URLS = process.env.FILE_LIST
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  e.respondWith(self.caches.match(e.request).then(function (request) {
-    if (request) return request
-    else return self.fetch(e.request)
-  }))
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return self.caches.match(event.request);
+    })
+  );
 })
+
 
 // Register worker
 self.addEventListener('install', function (e) {
