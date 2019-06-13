d4e6ad900bed850b25e1dc6c935372dac8d79a52
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-06-13 19:14:01 +0100

    Lol.

--- views/404.js
+++ views/404.js
@@ -1,6 +1,6 @@
 var html = require('choo/html')
 
-var TITLE = 'books - route not found'
+var TITLE = 'Route not found'
 
 module.exports = view
 
@@ -9,6 +9,9 @@ function view (state, emit) {
   return html`
     <body class="sans-serif pa3">
       <h1>Route not found.</h1>
+      <p>
+        Can't find what you're looking for? Use the link below!
+      </p>
       <a class="pt2" href="/">Back to main.</a>
     </body>
   `
